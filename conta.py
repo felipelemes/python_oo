fbbdeb08cf9a6e9505a968c1a5d5e1cfbadc75f3
Felipe Lemes <felipelemes@gmail.com>
2020-05-14 12:47:27 -0300

    Extrato, Deposita e Saca
    Adicionada as funções de extrato, depositar e sacar na classe Conta. Aprendendo a utlizar o self

--- conta.py
+++ conta.py
@@ -4,3 +4,16 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print(self.titular)
+        print(self.saldo)
+
+    def deposita(self,valor):
+        self.saldo += valor
+        print("Depósito realizado!")
+
+    def saca(self,valor):
+        self.saldo -= valor
+        print("Saque realizado!")
+
